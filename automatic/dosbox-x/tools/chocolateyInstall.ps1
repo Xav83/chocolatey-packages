3fa21cd99b3d75c931763f674487154802510bba
Chocolatey <chocolatey@realdimensions.net>
2022-01-10 05:37:20 +0000

    AU: 2 updated - cairoshell dosbox-x [skip ci] https://gist.github.com/c4eb33a8aa993629f3d047747322e809/3f093621fe27fbdd9576431e744abd8360a6d241

--- automatic/dosbox-x/tools/chocolateyInstall.ps1
+++ automatic/dosbox-x/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32       = 'https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.14/dosbox-x-windows-0.83.14-setup.exe'
-$url64       = ''
-$checksum32  = '9547250ac5db83cb1e2c10433df59ab2efdfa0c97b541199ea041f2b07d05df8'
-$checksum64  = ''
+$url32       = 'https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.21/dosbox-x-win32-0.83.21-setup.exe'
+$url64       = 'https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.21/dosbox-x-win64-0.83.21-setup.exe'
+$checksum32  = '840c13ef110567ea93a008a914c628b86632cbe30584b43b9b62aaab2b22a65e'
+$checksum64  = '34d57443902d0863e48bdd4637236e5851787c36dd1a8edfd0b18b7e080fce6c'
 
 $packageArgs = @{
   packageName    = $Env:ChocolateyPackageName
