b10e7ab9b668abdf537c6f4c281f3117fa2a855d
notFoxils <voltrixz9514@gmail.com>
2026-05-02 16:15:00 -0400

    fix(scraping): Fix the Rare package updater
    summary:
      - Change the update script to use the GitHub API.
      - Grab checksums from GitHub API instead of having Chocolatey
        calculate them.
      - Replace `packageArgs'` fields directly
    
    notes:
      - Needs testing, writing from a Linux machine so its not very easy

--- automatic/rare/update.ps1
+++ automatic/rare/update.ps1
@@ -1,35 +1,74 @@
 import-module chocolatey-au
 
-$releases = 'https://github.com/Dummerle/Rare/releases'
-
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"       = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
      }
 }
 
 function global:au_GetLatest {
-    $releases_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $release_tag_url = "https://github.com" + ($releases_page.Links.Href -match "/tag/" | Select-Object -First 1)
+    $latestRelease = Get-LatestRelease;
 
-    $expanded_assets_url = $release_tag_url -replace "/tag/","/expanded_assets/"
-    $assets_page = Invoke-WebRequest -Uri $expanded_assets_url -UseBasicParsing
+    $TARGET_ASSET_REGEX = 'Rare-.*-x86_64.msi'
 
-    $re  = "Rare-.+.msi"
-
-    $url = $assets_page.Links.Href -match $re | Select-Object -First 1
-
-    $version = $url -split '/' | Select-Object -Last 1 -Skip 1
-    if ($version.Contains("rc.")) { 
-        $version = $version -replace 'rc.','rc'
+    $targetAsset = $latestRelease.assets | Where-Object { $_.name -match $TARGET_ASSET_REGEX }
+    if (-not $targetAsset) {
+        Write-Host 'Unable to find the taget asset'
+        return 'ignore'
     }
-    $url64 = 'https://github.com' + $url
 
-    $Latest = @{ URL64 = $url64; Version = $version }
-    return $Latest
+    $url64 = $targetAsset.browser_download_url
+
+    $version = $latestRelease.tag_name
+    # NOTE: Clean-up rc tags
+    # Not sure if this is needed, the last rc was in 2022.
+    # Keeping this here for equivalence sake.
+    $version = $Version -creplace 'rc.', 'rc'
+
+    # EXAMPLE: $targetAsset.digest == "sha256:2151b604e..."
+    $checksumType64, $checksum64 = $targetAsset.digest -split ':', 2
+
+    return @{
+        URL64 = $url64
+        Checksum64 = $checksum64
+        ChecksumType64 = $checksumType64
+
+        Version = $version
+    }
 }
 
-update -ChecksumFor 64
+function Get-LatestRelease {
+    $REPO_OWNER = 'RareDevs'
+    $REPO_NAME = 'Rare'
+
+    $REQUEST_HEADERS = @{
+        'Accept' = 'application/vnd.github+json'
+        'X-GitHub-Api-Version' = '2026-03-10'
+    }
+
+    $repoReleases = Invoke-WebRequest `
+        # NOTE: More convoluted usage of releases API
+        # There is a dedicated api for pulling the latest release.
+        # We don't use it as does not report pre-releases (which the old script did).
+        -Uri "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases" `
+        -Headers $REQUEST_HEADERS `
+        # NOTE: Match GitHub API-docs example usage
+        # The cURL based examples use the -L flag allowing redirects (iwr does this by default).
+        # The maximum amount of redirects by default in cURL is 50 (iwr's default is 10).
+        # This flag here allows us to match that 50 redirect number.
+        -MaximumRedirection 50 `
+        -UseBasicParsing `
+        | ConvertFrom-Json
+    if (-not $repoReleases) {
+        Write-Host 'Unable to request latest releases'
+        return 'ignore'
+    }
+
+    return $repoReleases[0]
+}
+
+update -ChecksumFor none
